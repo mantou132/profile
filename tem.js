84bbb51415424d10b5a14a5dc11e5ac7fd955454
mantou <xianqiao.wang@huaban.com>
2016-01-13 21:38:03 +0800

    20160113

---
+++ tem.js
@@ -0,0 +1,30 @@
+'use strict'
+
+let diff = (text_1, text_2) => {
+	const ROWS_1 = text_1.split('\n');
+	const ROWS_2 = text_2.split('\n');
+	let diff = new Array();
+	let l_1 = ROWS_1.length;
+	let l_2 = ROWS_2.length;
+	let pending;
+
+	for (let row_1 = 0, row_2 = 0; row_1 < l_1 || row_2 < l_2; /*empty*/) {
+		if (ROWS_1[row_1] === ROWS_2[row_2]) {
+			diff.push(true);
+			row_1++, row_1++;
+		} else {
+			diff.push(false);
+			for (let tem = row_1 + 1; tem < l_1; tem++) {
+				if (ROWS_1[tem] === ROWS_2[row_2]) {
+
+				}
+			}
+		}
+	}
+
+	return diff;
+}
+
+let diff_row = (row_1, row_2) => {
+
+}
